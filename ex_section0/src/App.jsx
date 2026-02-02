09fd52ca8437548a0a6cfb014b746b43815b1937
joyh7680 <joy.h7680@gmail.com>
2026-02-02 16:48:32 +0900

    upload : 28 배경 유틸리티

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,8 +1,14 @@
 export default function App() {
   return (
     <>
-      {/* <div className="size-20 animate-spin bg-blue-500"></div> */}
-      <div className="size-20 animate-wiggle bg-blue-500"></div>
+      {/* <div className="size-50 bg-pink-500"></div> */}
+      {/* bg-black/50 : 투명도50  */}
+      {/* <div className="h-screen w-screen bg-black/50 bg-[url('/visionboard.jpg')] bg-cover bg-center bg-no-repeat"></div> */}
+
+      <div className="m-5 inline-block size-20 bg-linear-to-b from-blue-500 via-green-500 to-pink-500"></div>
+      <div className="m-5 inline-block size-20 bg-linear-to-r from-blue-500 to-pink-500"></div>
+      <div className="m-5 inline-block size-20 bg-linear-45 from-blue-500 to-pink-500"></div>
+      <div className="m-5 inline-block size-20 bg-radial from-blue-500 to-pink-500"></div>
     </>
   );
 }
