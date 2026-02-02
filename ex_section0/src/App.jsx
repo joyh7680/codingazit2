8723061c23ade61e5f2257bb31be65178b3365e4
joyh7680 <joy.h7680@gmail.com>
2026-02-02 19:10:53 +0900

    upload: sec3 30 border-utility

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,12 +1,7 @@
 export default function App() {
   return (
     <>
-      <div className="size-50 bg-blue-500"></div>
-      <div className="m-10 size-50 bg-red-500"></div>
-      {/* <div className="m-10 h-100 w-100 bg-blue-500">
-        <p className="text-4xl text-white">hellow</p>
-      </div> */}
-      {/* <div className="size-100 bg-blue-500"></div> */}
+      <div className="size-40 rounded-2xl border-2 border-dashed border-blue-500"></div>
     </>
   );
 }
