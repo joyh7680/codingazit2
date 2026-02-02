e07fbdb7d027ad161102e1129812f83ec67800d1
joyh7680 <joy.h7680@gmail.com>
2026-02-02 19:01:40 +0900

    upload:  29. 크기 여백 유틸리티

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,14 +1,12 @@
 export default function App() {
   return (
     <>
-      {/* <div className="size-50 bg-pink-500"></div> */}
-      {/* bg-black/50 : 투명도50  */}
-      {/* <div className="h-screen w-screen bg-black/50 bg-[url('/visionboard.jpg')] bg-cover bg-center bg-no-repeat"></div> */}
-
-      <div className="m-5 inline-block size-20 bg-linear-to-b from-blue-500 via-green-500 to-pink-500"></div>
-      <div className="m-5 inline-block size-20 bg-linear-to-r from-blue-500 to-pink-500"></div>
-      <div className="m-5 inline-block size-20 bg-linear-45 from-blue-500 to-pink-500"></div>
-      <div className="m-5 inline-block size-20 bg-radial from-blue-500 to-pink-500"></div>
+      <div className="size-50 bg-blue-500"></div>
+      <div className="m-10 size-50 bg-red-500"></div>
+      {/* <div className="m-10 h-100 w-100 bg-blue-500">
+        <p className="text-4xl text-white">hellow</p>
+      </div> */}
+      {/* <div className="size-100 bg-blue-500"></div> */}
     </>
   );
 }
