24545d5deae6977020961a368971aa7d557049f0
joyh7680 <joy.h7680@gmail.com>
2026-02-02 14:39:22 +0900

    upload: 섹션3 26.텍스트 관련 유틸리티

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,20 +1,11 @@
 export default function App() {
-  const info = {
-    name: "david",
-    age: 20,
-    gender: "male"
-  };
-
   return (
     <div>
-      <h1 className="text-4xl text-pink-500" onClick={() => console.log("test")}>
-        tailwindcss 4
-      </h1>
-      <ul>
-        <li>test1</li>
-        <li>test2</li>
-        <li>test3</li>
-      </ul>
+      <p className="font-sans">TEXT</p>
+      <p className="font-serif">TEXT</p>
+      <p className="font-mono">TEXT</p>
+      <p className="font-noto">TEXT</p>
+      <p className="font-orbitron">TEXT</p>
     </div>
   );
 }
