8201b8e6e7290cd5af2a338cfdf42979490b284d
joyh7680 <joy.h7680@gmail.com>
2026-02-03 16:16:48 +0900

    upload: 섹션3  49. TailwindCSS 스타일 관리 팁

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,14 +1,31 @@
+import React from 'react';
+const tw = str => str;
+
 export default function App() {
-    {/* motion-safe 
-      : 동작 줄이기(Reduce Motion)" 옵션이 비활성화되어 있을 때만 스타일을 적용하겠다는 의미
-    윈도우 설정 > 접근성 > 에니메이션효과 끔 => motion-safe 적용 => 애니메이션 효과 적용 안됨
-    */}
+  const css = {
+    article: tw`w-1/3 rounded-lg bg-white p-4 shadow-lg`,
+    h2: tw`mb-4 text-4xl font-bold`,
+    p: tw`mb-6 text-justify text-sm leading-5`,
+    span: tw`text-xs font-bold text-pink-500`
+  };
 
   return (
-    <>
-      <div className="size-60 bg-red-500 hover:bg-blue-500 theme-transition"></div>
-      {/* <div className="size-60 bg-red-500 hover:bg-blue-500 motion-safe:transition-colors motion-safe:duration-1000"></div> */}
-      {/* 코드마다 motion-safe 적용하는 것은 비효율적  */}
-    </>
+    <article className={css.article}>
+      <h2 className={css.h2}>title</h2>
+      <p className={css.p}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, vitae ad laudantium assumenda tenetur qui dolorum totam, ipsum
+        voluptate maiores officiis non rem exercitationem consequuntur impedit earum nam mollitia minima!
+      </p>
+      <span className={css.span}>2026.02.03</span>
+    </article>
+
+    // <article className="w-1/3 rounded-lg bg-white p-4 shadow-lg">
+    //   <h2 className="mb-4 text-4xl font-bold">title</h2>
+    //   <p className="mb-6 text-justify text-sm leading-5">
+    //     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, vitae ad laudantium assumenda tenetur qui dolorum totam, ipsum
+    //     voluptate maiores officiis non rem exercitationem consequuntur impedit earum nam mollitia minima!
+    //   </p>
+    //   <span className="text-xs font-bold text-pink-500">2026.02.03</span>
+    // </article>
   );
 }
