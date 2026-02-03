00cf8c8c3cd1a9d8f81c27f5f3e4a2da0e21801b
joyh7680 <joy.h7680@gmail.com>
2026-02-03 11:20:17 +0900

    upload: 섹션3 44. 레이어 스타일 시스템

--- ex_section0/src/App.jsx
+++ ex_section0/src/App.jsx
@@ -1,11 +1,12 @@
 export default function App() {
   return (
     <>
-      <ul className="space-y-5 *:p-5">
-        <li className="bg-my1">list1</li>
-        <li className="bg-my2">list2</li>
-        <li className="bg-my3">list3</li>
-      </ul>
+      <h1>Hello, World!</h1>
+
+      <button className="btn">btn</button>
+      <a href="/" className="btn">
+        BUTTON2
+      </a>
     </>
   );
 }
