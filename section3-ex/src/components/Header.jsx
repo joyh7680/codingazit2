47a27e54c8df55d84410db085ea30420adfca7c4
joyh7680 <joy.h7680@gmail.com>
2026-02-04 16:56:49 +0900

    fix : header 주석정리

--- section3-ex/src/components/Header.jsx
+++ section3-ex/src/components/Header.jsx
@@ -1,47 +1,15 @@
-
-import { Link, NavLink } from "react-router-dom";
-import Logo from "./items/Logo";
-import Gnb from "./items/Gnb";
-import Sns from "./items/Sns";
+import Logo from './items/Logo';
+import Gnb from './items/Gnb';
+import Sns from './items/Sns';
 
 export default function Header() {
-
-  // const gnb = ["Members", "Notice", "Gallery", "Youtube", "Contact"];
-  // const sns = [
-  //   { ico: FaFacebookSquare, path: "/facebook" },
-  //   { ico: FaEnvelope, path: "/mail" },
-  //   { ico: FaYoutube, path: "/youtube" },
-  // ];
-
   return (
     <header>
-      {/* logo로 이동  */}
-      {/* <h1>
-        <Link to="/"> CODING AZIT </Link>
-      </h1> */}
       <Logo />
 
       <nav>
-        {/* Gnb.jsx 로 이동  */}
         <Gnb />
-        {/* gnb  */}
-        {/* <ul>
-          {gnb.map((el, idx) => (
-            <li key={idx}>
-              <NavLink to={"/" + el.toLowerCase()}>{el}</NavLink>
-            </li>
-          ))}
-        </ul> */}
-
-        <span>
-          <Sns />
-          {/* sns */}
-          {/* {sns.map((el, idx) => (
-            <Link key={idx} to={el.path}>
-              <el.ico />
-            </Link>
-          ))} */}
-        </span>
+        <Sns />
       </nav>
     </header>
   );
--- section3-ex/src/components/items/Logo.jsx
+++ section3-ex/src/components/items/Logo.jsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import { tw } from '../../globals';
+import { twMerge } from 'tailwind-merge';
 
-export default function Logo() {
+// className 으로 css overwrite할수있게
+export default function Logo({ className = '' }) {
+  const css = {
+    h1: tw`font-orbitron text-xl font-semibold drop-shadow-md`
+  };
+
   return (
-     <h1>
+    <h1 className={twMerge(css.h1, className)}>
       <Link to="/"> CODING AZIT </Link>
     </h1>
-  )
+  );
 }
