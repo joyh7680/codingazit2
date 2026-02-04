c1a47f6a9021e6cac1d44df13b4ae0b0e5acee32
joyh7680 <joy.h7680@gmail.com>
2026-02-04 18:49:59 +0900

    feat : 섹션3 55. 실습 - 헤더영역 관리 컴포넌트 (Header)

--- section3-ex/src/components/Header.jsx
+++ section3-ex/src/components/Header.jsx
@@ -1,15 +1,27 @@
 import Logo from './items/Logo';
 import Gnb from './items/Gnb';
 import Sns from './items/Sns';
+import { tw } from '../globals';
+
+// 손이 많이 가는 단점이 있지만 유지보수, 재사용에 있어 리액트의 장점이 있다.
 
 export default function Header() {
+  const css = {
+    // px-[10vw] 좌우에 화면 전체 너비의 10% 여백을 주겠다
+    header: tw`fixed top-0 left-0 z-50 flex w-full items-center justify-between px-[10vw] py-5 shadow-sm shadow-dark/10 backdrop-blur-md`,
+    nav: tw`flex items-center gap-10`,
+    div: tw`flex gap-30 max-xl:hidden`
+  };
+
   return (
-    <header>
+    <header className={css.header}>
       <Logo className="text-point" />
 
-      <nav>
-        <Gnb />
-        <Sns />
+      <nav className={css.nav}>
+        <div className={css.div}>
+          <Gnb />
+          <Sns />
+        </div>
       </nav>
     </header>
   );
