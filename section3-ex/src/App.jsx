280dfb25377564a4762a0bed9990c270743de80d
joyh7680 <joy.h7680@gmail.com>
2026-02-05 19:07:49 +0900

    feat: 메인페이지에서는 footer ui가 안나오게 로직 구현

--- section3-ex/src/App.jsx
+++ section3-ex/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/blocks/Footer';
 import Header from './components/blocks/Header';
@@ -10,6 +10,8 @@ import Youtube from './pages/Youtube';
 import YoutubeDetail from './pages/YoutubeDetail';
 
 export default function App() {
+  // 현재 브라우저의 URL 경로 정보를 담고 있는 객체를 가져옵니다.
+  const location = useLocation();
   return (
     <>
       <Header />
@@ -24,7 +26,14 @@ export default function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
-      <Footer />
+      {/* 
+      특정 페이지에서만 푸터(Footer)를 숨기기 위한 조건부 렌더링 로직
+      location.pathname: 현재 접속한 주소를 의미
+      !== '/': "현재 주소가 /(메인 페이지)가 아니라면"이라는 조건
+      && (논리곱 연산자):
+      앞의 조건이 true일 때만 뒤에 있는 <Footer />를 화면에 그립니다.
+       */}
+      {location.pathname !== '/' && <Footer />}
     </>
   );
 }
